2e6ea64e3c7cd442775ba47fb34ea7261e5b4e2b
Tomasz Rysak <tomasz.rysak.ctr@sabre.com>
2022-05-06 07:53:38 +0200

    add FlightType component - RadioButton for chosing oneway or roundtrip

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,16 @@
 import "../../App.css";
 import { FormControl } from "@mui/material";
-import { buttonText } from "../../Translations/translations";
+import { buttonText, flightOptions } from "../../Translations/translations";
 import FlightRoute from "../FlightRoute/FlightRoute";
 import FlightType from "../FlightType/FlightType";
 import FlightDate from "../FlightDate/FlightDate";
 import SearchFlightButton from "../Buttons/SearchFlightButton";
+import React from "@types/react";
 
 const App = () => {
   return (
     <FormControl>
-      <FlightType />
+      <FlightType flightOptions={flightOptions} />
       <FlightRoute />
       <FlightDate />
       <SearchFlightButton buttonText={buttonText.SEARCH_FLIGHT} />
---
+++ src/Components/FlightType/FlightType.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import * as PropTypes from "prop-types";
+
+const FlightType = ({ flightOptions }) => {
+  const { ONE_WAY, ROUND_TRIP } = flightOptions;
+  const [value, setValue] = useState(ONE_WAY);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <RadioGroup
+      row
+      aria-labelledby="flight type selection"
+      value={value}
+      onChange={handleChange}
+      name="flight-type"
+    >
+      <FormControlLabel value={ONE_WAY} label={ONE_WAY} control={<Radio />} />
+      <FormControlLabel
+        value={ROUND_TRIP}
+        label={ROUND_TRIP}
+        control={<Radio />}
+      />
+    </RadioGroup>
+  );
+};
+
+FlightType.propTypes = {
+  flightRouteWays: PropTypes.shape({
+    ONE_WAY: PropTypes.string.isRequired,
+    ROUND_TRIP: PropTypes.string.isRequired,
+  }),
+};
+
+export default FlightType;
