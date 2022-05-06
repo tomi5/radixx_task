d264d7012073bbfbd442a30cd0bef21046bbb7a2
Tomasz Rysak <tomasz.rysak.ctr@sabre.com>
2022-05-06 07:51:35 +0200

    add translations

---
+++ src/Translations/translations.js
@@ -0,0 +1,26 @@
+export const flightOptions = {
+  ONE_WAY: "One way",
+  ROUND_TRIP: "Round trip",
+};
+
+export const flightRouteWays = {
+  TO: "To",
+  FROM: "From",
+};
+
+export const flightDateName = {
+  DEPART: "Depart",
+  RETURN: "Return",
+};
+
+export const buttonText = {
+  SEARCH_FLIGHT: "Search flight",
+};
+
+export const errors = {
+  FETCH_ERROR: "There is a problem witch fetching data",
+};
+
+export const loadingData = {
+  LoadingAirports: "A moment please...",
+};
